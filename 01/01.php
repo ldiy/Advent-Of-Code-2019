1984d001e3636906a25c4ebebffdaa33dc922033
Lorenz Clijnen <lorenz_reclame@outlook.be>
2019-12-01 10:44:58 +0100

    day 01

--- 01/01.php
+++ 01/01.php
@@ -1,3 +1,8 @@
 <?php
-
+$lines = file("input.txt", FILE_IGNORE_NEW_LINES);
+$sum = 0;
+foreach($lines as $line){
+	$sum += floor($line / 3) - 2;
+}
+echo $sum;
 ?>
---
+++ 01/02.php
@@ -0,0 +1,16 @@
+<?php
+function calc_extra_fuel($in){
+	if($in <= 0)
+		return(0);
+	else
+		return($in + calc_extra_fuel(floor($in / 3) -2));
+}
+$lines = file("input.txt", FILE_IGNORE_NEW_LINES);
+$sum = 0;
+
+foreach($lines as $line){
+	$fneeded = floor($line / 3) - 2;
+	$sum +=  calc_extra_fuel($fneeded);
+}
+echo $sum;
+?>
