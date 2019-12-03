854b2d5a6fa4b16495625eec43942676faddd9ca
Lorenz Clijnen <lorenz_reclame@outlook.be>
2019-12-03 19:16:32 +0100

    clean up

--- 03/01.php
+++ 03/01.php
@@ -5,68 +5,34 @@ $result = 10000000;
 $field = array(array());
 $path = 1;
 foreach($lines as $line){
-	$x=0;
-	$y=0;
+	$x=$y=0;
 	$instructions = explode(",",$line);
 	foreach($instructions as $instruction){
 		$val = substr($instruction,1);
-		//echo "val: " . $val . PHP_EOL;
-		switch($instruction[0]){
-			case "U":
-				//$y+=1;
-				for($i = 0; $i < $val; $i++){
-					$y++;
-					if($field[$x][$y] == NULL || $field[$x][$y] == $path)
-						$field[$x][$y] = $path;
-					else{
-						$field[$x][$y] = 'x';
-						$result = min(abs($x)+abs($y),$result);
-					}
+		for($i = 0; $i < $val; $i++){
+				switch($instruction[0]){
+					case "U":
+						$y++;
+						break;
+					case "D":
+						$y--;
+						break;
+					case "L":
+						$x--;
+						break;
+					case "R":
+						$x++;
+						break;
 				}
-				break;
-			case "D":
-				//$y-=1;
-				for($i = 0; $i < $val; $i++){
-					$y--;
-					if($field[$x][$y] == NULL || $field[$x][$y] == $path)
-						$field[$x][$y] = $path;
-					else{
-						$field[$x][$y] = 'x';
-						$result = min(abs($x)+abs($y),$result);
-					}
-				}
-				break;
-			case "L":
-				//$x-=1;
-				for($i = 0; $i < $val; $i++){
-					$x--;
-					if($field[$x][$y] == NULL || $field[$x][$y] == $path)
-						$field[$x][$y] = $path;
-					else{
-						$field[$x][$y] = 'x';
-						$result = min(abs($x)+abs($y),$result);
-					}
-				}
-				break;
-			case "R":
-				//$x+=1;
-				for($i = 0; $i < $val; $i++){
-					$x++;
-					//echo $x . PHP_EOL;
-					if($field[$x][$y] == NULL || $field[$x][$y] == $path)
-						$field[$x][$y] = $path;
-					else{
-						$field[$x][$y] = 'x';
-						$result = min(abs($x)+abs($y),$result);
-					}
-				}
-				break;
-		
+			if($field[$x][$y] == NULL || $field[$x][$y] == $path)
+				$field[$x][$y] = $path;
+			else{
+				$field[$x][$y] = 'x';
+				$result = min(abs($x)+abs($y),$result);
+			}
 		}
-		//echo "x: " . $x . " | y: " . $y . PHP_EOL;
 	}
 	$path++;
 }
-//print_r($field);
 echo $result;
 ?>
--- 03/02.php
+++ 03/02.php
@@ -4,132 +4,74 @@ $lines = file("input.txt", FILE_IGNORE_NEW_LINES);
 $result = 10000000;
 $field = array(array());
 $path = 1;
-
 function findOhterPath($x,$y,$myPath){
 	global $field;
-
 	if($field[$x+1][$y] != NULL && $field[$x+1][$y] != $myPath)
-		return $field[$x+1][$y];
-	
+		return $field[$x+1][$y];	
 	elseif($field[$x-1][$y] != NULL && $field[$x-1][$y] != $myPath)
-		return $field[$x-1][$y];
-	
+		return $field[$x-1][$y];	
 	elseif($field[$x][$y+1] != NULL && $field[$x][$y+1] != $myPath)
-		return $field[$x][$y+1];
-	
+		return $field[$x][$y+1];	
 	elseif($field[$x][$y-1] != NULL && $field[$x][$y-1] != $myPath)
-		return $field[$x][$y-1];
-	
-	
+		return $field[$x][$y-1];	
 }
-
 function calcDistance($xEnd,$yEnd,$pathNumber){
 	global $lines;
 	$line = $lines[$pathNumber-1];
-	$x=0;
-	$y=0;
+	$x=$y=$homedistance=0;
 	$instructions = explode(",",$line);
-	$homedistance = 0;
 	foreach($instructions as $instruction){
 		$val = substr($instruction,1);
-		switch($instruction[0]){
-			case "U":
-				for($i = 0; $i < $val; $i++){
+		for($i = 0; $i < $val; $i++){
+			switch($instruction[0]){
+				case "U":
 					$y++;
-					$homedistance++;
-					if($x == $xEnd && $y == $yEnd)
-						return $homedistance;
-				}
-				break;
-			case "D":
-				for($i = 0; $i < $val; $i++){
+					break;
+				case "D":
 					$y--;
-					$homedistance++;
-					if($x == $xEnd && $y == $yEnd)
-						return $homedistance;
-				}
-				break;
-			case "L":
-				for($i = 0; $i < $val; $i++){
+					break;
+				case "L":
 					$x--;
-					$homedistance++;
-					if($x == $xEnd && $y == $yEnd)
-						return $homedistance;
-				}
-				break;
-			case "R":
-				for($i = 0; $i < $val; $i++){
+					break;
+				case "R":
 					$x++;
-					$homedistance++;
-					if($x == $xEnd && $y == $yEnd)
-						return $homedistance;
-				}
-				break;
-		
-		}
+					break;
+			}
+			$homedistance++;
+			if($x == $xEnd && $y == $yEnd)
+				return $homedistance;
+		}			
 	}
 }
 foreach($lines as $line){
-	$x=0;
-	$y=0;
+	$x=$y=$homedistance=0;
 	$instructions = explode(",",$line);
-	$homedistance = 0;
 	foreach($instructions as $instruction){
 		$val = substr($instruction,1);
-		switch($instruction[0]){
-			case "U":
-				for($i = 0; $i < $val; $i++){
+		for($i = 0; $i < $val; $i++){
+			switch($instruction[0]){
+				case "U":
 					$y++;
-					$homedistance++;
-					if($field[$x][$y] == NULL || $field[$x][$y] == $path)
-						$field[$x][$y] = $path;
-					else{
-						$field[$x][$y] = 'x';				
-						$result = min($homedistance + calcDistance($x,$y,$otherPath = findOhterPath($x,$y,$path)) ,$result);
-					}
-				}
-				break;
-			case "D":
-				for($i = 0; $i < $val; $i++){
+					break;
+				case "D":
 					$y--;
-					$homedistance++;
-					if($field[$x][$y] == NULL || $field[$x][$y] == $path)
-						$field[$x][$y] = $path;
-					else{
-						$field[$x][$y] = 'x';				
-						$result = min($homedistance + calcDistance($x,$y,$otherPath = findOhterPath($x,$y,$path)) ,$result);
-					}
-				}
-				break;
-			case "L":
-				for($i = 0; $i < $val; $i++){
+					break;
+				case "L":
 					$x--;
-					$homedistance++;
-					if($field[$x][$y] == NULL || $field[$x][$y] == $path)
-						$field[$x][$y] = $path;
-					else{
-						$field[$x][$y] = 'x';				
-						$result = min($homedistance + calcDistance($x,$y,$otherPath = findOhterPath($x,$y,$path)) ,$result);
-					}
-				}
-				break;
-			case "R":
-				for($i = 0; $i < $val; $i++){
+					break;
+				case "R":
 					$x++;
-					$homedistance++;
-					if($field[$x][$y] == NULL || $field[$x][$y] == $path)
-						$field[$x][$y] = $path;
-					else{
-						$field[$x][$y] = 'x';				
-						$result = min($homedistance + calcDistance($x,$y,$otherPath = findOhterPath($x,$y,$path)) ,$result);
-					}
-				}
-				break;
-		
-		}
-		
-	}
-	
+					break;
+			}
+			$homedistance++;
+			if($field[$x][$y] == NULL || $field[$x][$y] == $path)
+				$field[$x][$y] = $path;
+			else{
+				$field[$x][$y] = 'x';				
+				$result = min($homedistance + calcDistance($x,$y,$otherPath = findOhterPath($x,$y,$path)) ,$result);
+			}
+		}	
+	}	
 	$path++;
 }
 echo $result;
